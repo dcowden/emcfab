b551921a9119fd8d91684bf479946e7c915d649c
dave.cowden <dave.cowden@4b45352a-18da-11df-8cb0-335a32c9b69e>
2010-02-13 21:05:42 +0000

    stupid thing didnt handle binary correctly

--- trunk/applications/firmware/extruderControler/duty.h
+++
@@ -1,8 +0,0 @@
-//header file for duty cycle control
-
-
-
-void initDuty( unsigned short dtyperiod );
-void dutyInterruptProc(void);
-void setDuty( unsigned short setpnt );
-
--- trunk/applications/firmware/extruderControler/eeprom.h
+++
@@ -1,6 +0,0 @@
-typedef unsigned char byte;
-typedef unsigned int word;
-
-
-void Eeprom_Write_Obj(word addr,void *obj,byte size);
-void Eeprom_Read_Obj(word addr,void *obj,byte size);
